f29ed2a46e95589f9aed55d27f4c433ccd10d3c3
Sam Messina <samuel.messina@gmail.com>
2015-10-24 02:50:26 -0500

    a little cleanup and commenting

--- URLReader.java
+++ URLReader.java
@@ -7,7 +7,7 @@ public class URLReader {
 
     public static void main(String[] args) throws Exception {
 
-        URL oracle = new URL("http://localhost/test.html");
+        URL oracle = new URL("https://www.twitter.com/");
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(oracle.openStream()));
 
@@ -19,52 +19,86 @@ public class URLReader {
         boolean spaceReached;
         char[] inputArray;
 
-        while ((inputLine = in.readLine()) != null) {
+        while ((inputLine = in.readLine()) != null) { // Go through each line
+
           closingTag = "";
           openingTag = "";
-          inputArray = inputLine.toCharArray();
+          inputArray = inputLine.toCharArray(); 
+          // I found it easier to iterate through an array of characters to look for tags.
+
           for (int i = 0; i < inputArray.length; i++) {
+
             if (inputArray[i] == '<') {
               if (inputArray[i + 1] == '/') {
-                closingTag += "</";
-                for (int j = i+2; j < inputArray.length; j++) {
-                  if (inputArray[j] == '>') {
-                    closingTag += inputArray[j];
-                    break;
-                  }
-                  else {
-                    closingTag += inputArray[j];
-                  }
+                closingTag += "<"; 
+                // closing tag is stored without the '/' so it can be easily compared to the opening.
 
+                for (int j = i+2; j < inputArray.length; j++) {
+                  // Since the first two characters, "</", are accounted for, we start at the third
+                    closingTag += inputArray[j];
+
+                  if (inputArray[j] == '>') {
+                    break;
+                  } //end if
+
+                } // end for
+
+
+                // Check the last opening tag after a closing tag has been found 
+                // This section still needs some work
+                if (closingTag.equals(openingTag)) {
+                  System.out.println(openingTag + " -- " + closingTag);
+                } //end if
+                else {
+                  System.out.println("Uh oh....");
+                  System.out.println("-------------------");
+                  System.out.println(openingTag + " -- " + closingTag);
+                  System.out.println("-------------------");
                 }
-              }
+
+              } //end if
+
+
               else {
+                // else, it must be an opening tag.
+
                 openingTag += "<";
                 spaceReached = false;
+                // spaceReached is used to find the end of the tag name and the start of 
+                // things like id, name, etc. We dont want to store these things, so
+                // we can just skip over them.
+
                 for (int j = i+1; j < inputArray.length; j++) {
+                  // Since "<" has already been acounted for, we don't can start at i+1
+
                   if (inputArray[j] == ' ') {
+                    //check for the spacereached
+
                     spaceReached = true;
-                  }
-                  if (inputArray[j] == '>') {
+                  } //end if
+
+                  if (inputArray[j] == '>' && inputArray[j-1] != '/') {
+                    // This finds the end of the tag, and only pushes it if it is
+                    // not a self-closing tag like <meta>, <br>, etc.
+
+                      openingTag += inputArray[j];
+                      s.push(openingTag);
+                      break;
+                  } //end if
+
+                  else if (!spaceReached) {
+                    // If it's not the end of the tag and a space has not been found, 
+                    // the string is updated with the new character
+
                     openingTag += inputArray[j];
-                    s.push(openingTag);
-                    break;
-                  }
-                  if (!spaceReached) {
-                    openingTag += inputArray[j];
-                  } 
-                }
-              }
-            }
+                  }  //end if
+                } // end for
+              } //end else
+            } // end if
 
-          }
+          } //end for
 
-
-        }
-
-        while (!s.isEmpty()) {
-          System.out.println(s.pop());
-        }
+        } //end while
 
 
 
@@ -119,6 +153,10 @@ public class URLReader {
           We should design the program to find tags regardless of their text. The
           most obvious way to do this is to look for the angle brackets <>.
 
+          My above code seems to be working for a normal html file. There are some 
+          edge cases we'll have to take into account. It's throwing a lot of errors
+          for big sites like Twitter, but working for basic html files. I'm a beer
+          too far in to fix it right now, but we're at least moving forward.
 
 
          */
