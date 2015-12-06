24e3215c0f8ce4100412d227d51255cdbbcca8fb
Sam Messina <samuel.messina@gmail.com>
2015-12-06 02:29:35 -0600

    final commit! All comments and everything done

--- Parser.java
+++ Parser.java
@@ -9,100 +9,15 @@ public class Parser {
     this.websiteURL = websiteURL;
   }
 
-  public String[] parseSiteForErrors() throws Exception{
-
-    String HTMLOutput = "";
-    String errorOutput = "";
-
-    URL oracle = new URL(websiteURL);
-    BufferedReader in = new BufferedReader(
-            new InputStreamReader(oracle.openStream()));
-
-    Stack<String> s = new Stack<String>();
-
-    boolean errorFound = false;
-    String errorFlag = null;
-    String closingTag;
-    String openingTag;
-    String inputLine;
-    char[] inputArray;
-
-    int counter = 0;
-    while ((inputLine = in.readLine()) != null) { // Go through each line
-      if (errorFound) {
-        HTMLOutput += errorFlag + "\n";
-      }
-      HTMLOutput += inputLine + "\n";
-      errorFound = false;
-
-      counter++;
-      closingTag = "";
-      openingTag = "";
-
-      inputArray = inputLine.toCharArray();
-      // I found it easier to iterate through an array of characters to look for tags.
-
-      for (int i = 0; i < inputArray.length; i++) {
-        if (inputArray[i] == '<') {
-          if (inputArray[i + 1] == '/') {
-            // if the second character is a /, it must be a closing tag
-
-            if (s.size() != 0) {
-              openingTag = s.pop();
-              // pop off the opening tag for comparison
-            }
-            else {
-              openingTag = "";
-            }
-
-            closingTag = formatClosingTag(inputArray, i);
-
-            if (!closingTag.equals(openingTag)) {
-              errorOutput += "\nError Line " + counter +"....";
-              errorOutput += "-------------------";
-              errorOutput += openingTag + " -- " + closingTag;
-              errorOutput += "-------------------\n";
-              errorFound = true;
-              errorFlag = flagErrors(openingTag, closingTag);
-              counter += 6;
-              //counter has to account for the errorFlag text
-
-            } // end if
-
-          } //end if
-
-          else {
-            // else, it must be an opening tag.
-            openingTag = formatOpeningTag(inputArray, i);
-            if (openingTag != null) {
-              s.push(openingTag);
-            }
-          } //end else
-        } // end if
-      } //end for
-
-    } //end while
-    while (s.size() > 0) {
-      openingTag = s.pop();
-      errorOutput += "\nError ";
-      errorOutput += "-------------------";
-      errorOutput += openingTag + " Was no closed ";
-      errorOutput += "-------------------\n";
-
-    }
-    if (errorOutput.length() == 0) {
-      errorOutput += "No errors found! Well done!";
-    }
-    String[] outputArray = {HTMLOutput, errorOutput};
-    return outputArray;
-
-
-    //Opens the text file in default text editor
-
-    //path to file
-  }
 
   public static boolean isSelfClosing(String openingTag) {
+    /*
+     * This method takes and opening tag and makes sure it isn't included in W3's list
+     * of self-closing, or void tags.
+     *
+     * @param Takes in the tag.
+     * @return returns true if the tag is self-closing, false if it is not.
+     */
     String[] selfClosingTagArray = {"<area>", "<base>",  "<br>", "<br />", "<br/>", "<col>",
             "<embed>", "<hr>","<hr/>", "<hr /", "<img>", "<input>", "<keygen>", "<link>",
             "<menuitem>", "<meta>", "<param>", "<source>", "<track>", "<wbr>"};
@@ -125,23 +40,41 @@ public class Parser {
 
 
   public static String formatOpeningTag(char[] inputArray, int i){
+    /*
+     * After parseSiteForErrors finds what it thinks is an opening tag,
+     * this method takes over and formats the tag so it can be pushed to
+     * the stack.
+     *
+     * @param The current line's char array is taken in, along with the
+     *        starting point of the opening tag.
+     * @return The opening tag is returned if it is found to be able to
+     *         be pushed to the stack, otherwise null is returned.
+     */
+
     String openingTag = "";
+    // The tag itself.
     openingTag += "<";
+    // We can start it by adding the "<" for it.
+
     boolean spaceReached = false;
-    // spaceReached is used to find the end of the tag name and the start of 
-    // things like id, name, etc. We dont want to store these things, so
-    // we can just skip over them.
+    /*
+     * spaceReached is used to find the end of the tag name and the start of
+     * things like id, name, etc. We don't want to store these things, so
+     * we can just skip over them.
+    */
 
     for (int j = i+1; j < inputArray.length; j++) {
-      // Since "<" has already been acounted for, we don't can start at i+1
+      // Since "<" has already been accounted for, we can start at i+1
 
       if (inputArray[j] == ' ') {
-        //check for the spacereached
+        //check for spacereached
 
         spaceReached = true;
       } //end if
 
       if (inputArray[j] == '>' && openingTag.charAt(1)=='!') {
+        // If the tag closes, but the second character is a "!", it is a comment
+        // We can return null and exit the method.
         return null;
       }
 
@@ -150,27 +83,29 @@ public class Parser {
         // not a comment.
 
         openingTag += inputArray[j];
+        // Add the ">".
 
         if (!isSelfClosing(openingTag)) {
           // Validate that the tag is not self closing before pushing
-          return openingTag;
-          //System.out.println(openingTag);
-          // Reset the tags to allow for mutiple tags per row
+          break;
+          // Exit the for loop
         } //end if
         else {
           return null;
+          // If it is self closing, simply return null.
         }
-
       } //end if
 
       else if (!spaceReached) {
-        // If it's not the end of the tag and a space has not been found, 
+        // If it's not the end of the tag and a space has not been found,
         // the string is updated with the new character
 
         openingTag += inputArray[j];
       }  //end if
     } // end for
-    if (openingTag.length() < 2 || openingTag.charAt(1)=='!' || openingTag != "<" || openingTag != "<=" ) {
+    if (openingTag.length() < 2 || openingTag.charAt(1)=='!' ) {
+      // One last check outside the for loop. This if fixed false tags
+      // coming from less than symbols in inline javascript.
       openingTag = null;
     }
     return openingTag;
@@ -178,6 +113,15 @@ public class Parser {
 
 
   public static String formatClosingTag(char[] inputArray, int i){
+    /*
+     * This works just like formatOpeningTag, but for closing tags.
+     * Closing tags are easier, since they can't be self-closing and
+     * don't have attributes like id, class, etc.
+     *
+     * @param The current line's char array is taken in, along with the
+     *        starting point of the closing tag.
+     * @return The closing tag is returned.
+     */
     String closingTag = "<";
     // closing tag is stored without the '/' so it can be easily compared to the opening.
 
@@ -195,6 +139,14 @@ public class Parser {
 
 
   public static String flagErrors(String openingTag, String closingTag) {
+    /*
+     * This method just takes the mismatched opening and closing tags and
+     * formats an HTML comment that we can put in our returned file, making
+     * it easier to debug.
+     *
+     * @param The opening and closing tags that did not match.
+     * @return The HTML comment detailing the error.
+     */
     String result = "\n<!--\n   --Error Found on Line Above--\n" +
             "Found opening tag " + openingTag + " closed by " + closingTag +
             "\nPlease fix and try again\n-->\n";
@@ -202,4 +154,140 @@ public class Parser {
   }
 
 
+  public String[] parseSiteForErrors() throws Exception{
+    /*
+     * This method is the main method of Parser. It calls all other methods.
+     * It will parse the html file found from websiteURL, and record all
+     * errors.
+     *
+     * @param No parameter, as websiteURL is set in Parser's constructor
+     * @return A String array is returned. The first element in the array
+     *         is the HTML file, with errors recorded as comments in it.
+     *         The second element is a String with only the errors and line
+     *         numbers listed.
+     */
+
+    String HTMLOutput = "";
+    // HTMLOutput is the HTML file with errors commented in
+    String errorOutput = "";
+    // errorOutput is the text with just the errors in it
+
+    URL oracle = new URL(websiteURL);
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(oracle.openStream()));
+    // Open the site for reading
+
+    Stack<String> s = new Stack<String>();
+    // s is our stack. It stores opening tags as they are found.
+
+    boolean errorFound = false;
+    // errorFound is triggered when an error is found on a line.
+
+    String errorFlag = null;
+    // errorFlag is the comment written to the HTML file.
+
+    String closingTag;
+    // closingTag stores the closing tag as it is being parsed.
+
+    String openingTag;
+    // openingTag stores the opening tag as it is being parsed.
+
+    String inputLine;
+    // inputLine stores the lines of the HTML as they come up
+
+    char[] inputArray;
+    // inputArray will be a char array version of inputLine, so we
+    // can parse character by character.
+
+    int counter = 0;
+    // Counter is a line counter. It corresponds to the output html file, not
+    // the original
+
+    while ((inputLine = in.readLine()) != null) { // Go through each line
+      if (errorFound) {
+        // if an error was found on the previous line, add the errorFlag to HTMLOutput
+        HTMLOutput += errorFlag + "\n";
+        errorFlag = "";
+      }
+      HTMLOutput += inputLine + "\n";
+      // Add the next line to our HTMLOutput
+      errorFound = false;
+      // Make sure errorFound is set to false at the beginning of each line.
+
+      counter++;
+      // Increment counter for the new line.
+
+      inputArray = inputLine.toCharArray();
+      // I found it easier to iterate through an array of characters to look for tags.
+
+      for (int i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] == '<') {
+          // If we find a "<", we can check if it's a tag.
+
+          if (inputArray[i + 1] == '/') {
+            // if the second character is a /, it must be a closing tag
+
+            if (s.size() != 0) {
+              openingTag = s.pop();
+              // pop off the opening tag for comparison, but only if one has been pushed
+            }
+            else {
+              openingTag = "";
+            }
+
+            closingTag = formatClosingTag(inputArray, i);
+            // Call Parser's formatClosingTag method to create our closing tag.
+
+            if (!closingTag.equals(openingTag)) {
+              // Check if the closing tag matches the last pushed opening tag.
+              // If not, we add the errors.
+
+              errorOutput += "\nError Line " + counter +"....\n";
+              errorOutput += "-------------------\n";
+              errorOutput += openingTag + " -- " + closingTag + "\n";
+              errorOutput += "-------------------\n";
+              // Add an error to our errorOutput.
+
+              errorFound = true;
+              // Set errorFound to true so we can add our errorFlag.
+
+              errorFlag = flagErrors(openingTag, closingTag);
+              // Call Parser's flagErrors method, which formats
+              // a nice comment for HTMLOutput.
+
+              counter += 7;
+              //counter has to account for the errorFlag text
+
+            } // end if
+          } //end if
+
+          else {
+            // else, it must be an opening tag.
+            openingTag = formatOpeningTag(inputArray, i);
+            if (openingTag != null) {
+              s.push(openingTag);
+            } // end if
+          } //end else
+        } // end if
+      } //end for
+
+    } //end while
+    while (s.size() > 0) {
+      // Check for any extra tags that haven't been closed.
+      openingTag = s.pop();
+      errorOutput += "\nError \n";
+      errorOutput += "-------------------";
+      errorOutput += openingTag + " Was not closed\n";
+      errorOutput += "-------------------\n";
+    } // end while
+
+    if (errorOutput.length() == 0) {
+      // If there were no errors, we congratulate the user. Kudos!
+      errorOutput += "No errors found! Well done!";
+    }
+
+    String[] outputArray = {HTMLOutput, errorOutput};
+    // return the HTMLOutput and errorOutput to the user.
+    return outputArray;
+  }
 }
--- URLReader.java
+++ URLReader.java
@@ -1,15 +1,25 @@
 import java.awt.*;
 import java.io.*;
-import java.util.Scanner;
 
 public class URLReader {
 
 
-  public static void printToTextFile(String filename, String textToWrite) throws Exception{
+  public static void printToTextFile(String filename, String textToWrite)
+          throws Exception{
+    /*
+     * This method creates and populates a text file with a given string.
+     * It is used to store our programs output to a text file for easier
+     * reading.
+     *
+     * @param The name of the file and the text to write to it.
+     * @return There is no return from this method, the file is
+     *         simply stored to the user's computer.
+     */
 
     File myFile = new File(filename);
 
     if(!myFile.exists()) {
+      // Create the file if it does not exist.
         myFile.createNewFile();
     }
 
@@ -20,13 +30,22 @@ public class URLReader {
     String line = null;
 
     while ((line = br.readLine()) != null) {
+      // Write each line to the file
       writer.println(line);
     }
 
     writer.close();
+    // Close out the file after completion.
   }
 
-    public static boolean openHTMLFile(String fileName) throws IOException {
+
+    public static boolean openFile(String fileName) throws IOException {
+      /*
+       * This method opens a given file in the user's default text editor.
+       *
+       * @param The name of the file to open.
+       * @return Boolean value true if the method completes without error
+       */
       File file = new File(fileName);
       if(!file.exists()) {
           file.createNewFile();
@@ -43,29 +62,35 @@ public class URLReader {
       return true;
     }
 
-    public static String getURL(){
-      System.out.println("Enter web address below. Note: requires hhtp header");
-      Scanner scan = new Scanner(System.in);
-      String url = scan.nextLine();
-      return url;
-    }
+
     public static void main(String[] args) throws Exception {
 
+      Parser siu = new Parser("http://www.siu.edu");
+      Parser craigslist = new Parser("https://carbondale.craigslist.org/");
+      Parser samsWebsite = new Parser("http://woodyatrandom.com/filmography.php");
+      Parser reddit = new Parser("https://www.reddit.com/");
 
+      // A new Parser object is created. This object will parse our site for errors.
 
-      //Parser website = new Parser(getURL());
-      Parser website = new Parser("http://www.imdb.com/title/tt0113243/");
-      String[] results = website.parseSiteForErrors();
+      String[] results = samsWebsite.parseSiteForErrors();
+      // Check the site for errors. See Parser for details on the method.
+      // Note: change the Parser object called here to change the site that is checked.
+
       String checkedSite = results[0];
+      // The first element is the HTML text
+
       String errors = results[1];
-      String filename = "RawHTML.txt";
+      // The second element is the errors
+
+      String filename = "RawHTML.html";
+      // Write to and open the html file
       printToTextFile(filename, checkedSite);
-      openHTMLFile(filename);
+      openFile(filename);
+
       filename = "Errors.txt";
+      // Write to and open the error file
       printToTextFile(filename, errors);
-      openHTMLFile(filename);
+      openFile(filename);
 
     }
 }
-
-
